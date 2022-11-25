dfeff4fe32ce1a13ce058996c4ee536d18156915
Serhat Kumas <serhat.kumas@hotmail.com>
2022-11-26 01:58:38 +0300

    Code is commented

--- src/sliding.js
+++ src/sliding.js
@@ -1,51 +1,55 @@
-// function([string1, string2],target id,[color1,color2])    
-consoleText(['Parameter1', 'Parameter3', 'Parameter3'], 'text',['tomato','rebeccapurple','lightblue']);
+//Sliding text animation by the time 
+consoleText(['Parameter1', 'Parameter3', 'Parameter3'], 'text', ['tomato', 'rebeccapurple', 'lightblue']);
 
+//Writer function
 function consoleText(words, id, colors) {
-  if (colors === undefined) colors = ['#fff'];
-  var visible = true;
-  var con = document.getElementById('console');
-  var letterCount = 1;
-  var x = 1;
-  var waiting = false;
-  var target = document.getElementById(id)
-  target.setAttribute('style', 'color:' + colors[0])
-  window.setInterval(function() {
+    if (colors === undefined) colors = ['#fff'];
+    var visible = true;
+    var con = document.getElementById('console');
+    var letterCount = 1;
+    var x = 1;
+    var waiting = false;
+    var target = document.getElementById(id)
+    target.setAttribute('style', 'color:' + colors[0])
 
-    if (letterCount === 0 && waiting === false) {
-      waiting = true;
-      target.innerHTML = words[0].substring(0, letterCount)
-      window.setTimeout(function() {
-        var usedColor = colors.shift();
-        colors.push(usedColor);
-        var usedWord = words.shift();
-        words.push(usedWord);
-        x = 1;
-        target.setAttribute('style', 'color:' + colors[0])
-        letterCount += x;
-        waiting = false;
-      }, 100)
-    } else if (letterCount === words[0].length + 1 && waiting === false) {
-      waiting = true;
-      window.setTimeout(function() {
-        x = -1;
-        letterCount += x;
-        waiting = false;
-      }, 1000)
-    } else if (waiting === false) {
-      target.innerHTML = words[0].substring(0, letterCount)
-      letterCount += x;
-    }
-  }, 120)
-  window.setInterval(function() {
-    if (visible === true) {
-      con.className = 'console-underscore hidden'
-      visible = false;
+    //
+    window.setInterval(function() {
+        if (letterCount === 0 && waiting === false) {
+            waiting = true;
+            target.innerHTML = words[0].substring(0, letterCount)
+            window.setTimeout(function() {
+                var usedColor = colors.shift();
+                colors.push(usedColor);
+                var usedWord = words.shift();
+                words.push(usedWord);
+                x = 1;
+                target.setAttribute('style', 'color:' + colors[0])
+                letterCount += x;
+                waiting = false;
+            }, 100) // Cool off before writing next text
+        } else if (letterCount === words[0].length + 1 && waiting === false) {
+            waiting = true;
+            window.setTimeout(function() {
+                x = -1;
+                letterCount += x;
+                waiting = false;
+            }, 1000) // Cool off before deleting text
+        } else if (waiting === false) {
+            target.innerHTML = words[0].substring(0, letterCount)
+            letterCount += x;
+        }
+    }, 120) //Every character writing time
 
-    } else {
-      con.className = 'console-underscore'
+    //
+    window.setInterval(function() {
+        if (visible === true) {
+            con.className = 'console-underscore hidden'
+            visible = false;
 
-      visible = true;
-    }
-  }, 400)
+        } else {
+            con.className = 'console-underscore'
+
+            visible = true;
+        }
+    }, 400)
 }
